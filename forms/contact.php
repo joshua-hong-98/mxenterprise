2fe6d8c361937d6d2d0e594cc3c090a8279472b4
zeeodd <evergreenskier@gmail.com>
2020-07-13 13:30:28 -0700

    1. Updated PHP code 2. Threw in a new color pal 3. Modified the favicons

--- forms/contact.php
+++ forms/contact.php
@@ -12,14 +12,14 @@ if(empty($_POST['name'])      ||
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$subject = strip_tags(htmlspecialchars($_POST['phone']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'ziyad.duron.games@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "MX Enterprise Contact Form:  $name";
-$email_body = "You have received a new message from the Lake Los Serranos contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@lakelosserranos.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "You have received a new message from the MX Enterprise contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@mxenterprise.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
