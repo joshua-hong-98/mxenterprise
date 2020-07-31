b19682e7b48a237ecca54f533a3e57ec56ca80e2
zeeodd <evergreenskier@gmail.com>
2020-07-31 15:48:43 -0700

    Form Fix Pt 2

--- js/contact.js
+++ js/contact.js
@@ -19,9 +19,8 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      jQuery.support.cors = true;
       $.ajax({
-        url: "././forms/contact.php",
+        url: "https://mxindustrialcorp.com/forms/contact.php",
         type: "POST",
         data: {
           name: name,
