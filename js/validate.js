d20e74a732bb023ba7c63e6d54226198684b01df
zeeodd <evergreenskier@gmail.com>
2020-07-31 18:47:28 -0700

    Another attempt

--- js/validate.js
+++ js/validate.js
@@ -8,13 +8,13 @@
 
   $('form.php-email-form').submit(function(e) {
     e.preventDefault();
-    
+
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
-     
+
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -104,7 +104,7 @@
       this_form.find('.error-message').slideDown().html('The form action property is not set!');
       return false;
     }
-    
+
     this_form.find('.sent-message').slideUp();
     this_form.find('.error-message').slideUp();
     this_form.find('.loading').slideDown();
@@ -119,11 +119,12 @@
     } else {
       php_email_form_submit(this_form,action,this_form.serialize());
     }
-    
+
     return true;
   });
 
   function php_email_form_submit(this_form, action, data) {
+    console.log(action);
     $.ajax({
       type: "POST",
       url: action,
